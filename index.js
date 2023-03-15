307ce6d2d9062fcf4873071eba5298355b7c58e1
Lauren Giordano <laurengiordano94@gmail.com>
2023-03-15 12:36:29 +1100

    done with lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft () {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+
+function moveDodgerRight () {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+
+
+document.addEventListener("keydown", e => {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+            moveDodgerRight();
+        };
+    
+});
